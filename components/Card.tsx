1c9e0c72a05ea3887290521593291c5ed3266a30
DonzTea <donirahmatiana@gmail.com>
2022-07-04 19:58:43 +0700

    fix card style

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,11 +7,9 @@ interface Props {
 
 export default function Card({ children, title }: Props) {
   return (
-    <section
-      className={`w-full p-6 bg-white rounded-md shadow-md dark:bg-gray-800`}
-    >
+    <section className={`w-full p-6 bg-white rounded-md shadow-md`}>
       {title && (
-        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white mb-4">
+        <h2 className="text-lg font-semibold text-gray-700 capitalize mb-4">
           {title}
         </h2>
       )}
--- components/pages/index/TransferCard.tsx
+++ components/pages/index/TransferCard.tsx
@@ -35,26 +35,26 @@ export default function TransferCard(): ReactElement {
     <Card title="Transfer Form">
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
         <div>
-          <label className="text-gray-700 dark:text-gray-200" htmlFor="address">
+          <label className="text-gray-700" htmlFor="address">
             Recepient Address
           </label>
           <input
             ref={recipientInput}
             id="address"
             type="text"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
           />
         </div>
 
         <div>
-          <label className="text-gray-700 dark:text-gray-200" htmlFor="amount">
+          <label className="text-gray-700" htmlFor="amount">
             Amount
           </label>
           <input
             ref={amountInput}
             id="amount"
             type="number"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
           />
         </div>
       </div>
